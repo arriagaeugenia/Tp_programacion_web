c747bda4f1f7a335064a3110fe8bb36589952032
arriagaeugenia <marriaga@alumnos.exa.unicen.edu.ar>
2025-09-04 16:48:00 -0300

    finalizamos con exito el tp1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module galeriadearte.com
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	staticDir := "./html"
+	// Servidor de archivos
+	http.HandleFunc("/", inicio)
+
+	// Puerto
+	port := ":8080"
+	fmt.Printf("Servidor escuchando en http://localhost%s\n", port)
+	fmt.Printf("Sirviendo archivos desde: %s\n", staticDir)
+
+	// Levantar servidor
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Printf("Error al iniciar el servidor: %s\n", err)
+	}
+
+}
+func inicio(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	// Carpeta de archivos estáticos
+	fmt.Print(w, "./html")
+}
