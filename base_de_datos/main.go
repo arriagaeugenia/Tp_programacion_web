508ea20c2c2a91ad12dfff8e22d9f2c7231e095b
Milagros <milopez@alumnos.exa.unicen.edu.ar>
2025-09-23 17:44:08 -0300

    Servidor terminado, falta base de datos

--- base_de_datos/main.go
+++ base_de_datos/main.go
@@ -7,38 +7,41 @@ import (
 	"database/sql" // para sql.Open()
 	"fmt"          // para fmt.Println()
 	"log"
+	"strconv"
 
 	_ "github.com/lib/pq"
 )
-func main(){
-    connStr := "postgresql://milibianeuge:programacionweb@localhost:5432/db?sslmode=disable"
-      db, err := sql.Open("postgres", connStr)
-    if err != nil {
-        log.Fatalf("failed to connect to DB: %v", err)
-    }
-    defer db.Close()
-    queries := sqlc.New(db)
-    ctx := context.Background()
 
-    /*createdUser, err := queries.CreateObra(ctx, // Create
-    sqlc.CreateObraParams{
-        Titulo:     "La noche estrellada",
-        Descripcion: sql.NullString{String: "Una pintura famosa", Valid:true},
-        Artista:    "Vincent van Gogh",
-        Precio:      strconv.Itoa(1000000), 
-        Vendida:     sql.NullBool{Bool: false, Valid: true},
-    })
-    if err != nil {
-        log.Fatalf("failed to create obra: %v", err)
-    }
-    fmt.Printf("Created obra: %+v\n", createdUser)
+func main() {
+	connStr := "postgresql://milibianeuge:programacionweb@localhost:5432/db?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("failed to connect to DB: %v", err)
+	}
+	defer db.Close()
+	queries := sqlc.New(db)
+	ctx := context.Background()
 
-    obra, err := queries.GetObraById(ctx, createdUser.ID) // Read One
-    if err != nil {
-        log.Fatalf("failed to get obra: %v", err)
-    }
-    fmt.Printf("Retrieved obra: %+v\n", obra)*/
+	createdObra, err := queries.CreateObra(ctx, // Create
+		sqlc.CreateObraParams{
+			Titulo:      "La noche estrellada",
+			Descripcion: sql.NullString{String: "Una pintura famosa", Valid: true},
+			Artista:     "Vincent van Gogh",
+			Precio:      strconv.Itoa(1000000),
+			Vendida:     sql.NullBool{Bool: false, Valid: true},
+		})
+	if err != nil {
+		log.Fatalf("failed to create obra: %v", err)
+	}
+	fmt.Printf("Created obra: %+v\n", createdObra)
 
+	obra, err := queries.GetObraById(ctx, createdObra.ID) // Read One
+	if err != nil {
+		log.Fatalf("failed to get obra: %v", err)
+	}
+	fmt.Printf("Retrieved obra: %+v\n", obra)
+
+	//Actualiza la obra de "La noche estrellada"
 	err = queries.UpdateObra(ctx, sqlc.UpdateObraParams{ // Update
 		ID:          1,
 		Titulo:      "La noche estrellada!",
